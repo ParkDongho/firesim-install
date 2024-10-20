b9003b5489a2f9d309e2fb9bc66d03cd78329fbe
Dongho Park <parkdongho28@naver.com>
2024-10-20 15:04:57 +0900

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -35,9 +35,9 @@ sudo ./install_drivers
 
 echo "4.  Install the Xilinx XDMA and XVSEC drivers"
 cd ~/   # 또는 작업하길 원하는 디렉토리
-git clone https://github.com/ParkDongho/dma_ip_drivers/tree/nitefury
+git clone https://github.com/ParkDongho/dma_ip_drivers
 cd dma_ip_drivers
-git checkout 0e8d321
+git checkout nitefury
 cd XDMA/linux-kernel/xdma
 sudo make install
 sudo insmod $(find /lib/modules/$(uname -r) -name "xdma.ko") poll_mode=1
