d7cd8ee7ae38661b35ca2d8d1a11813b5ddd2eef
Dongho Park <parkdongho28@naver.com>
2024-10-22 15:48:04 +0900

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -17,8 +17,3 @@ sudo chgrp firesim /usr/local/bin/firesim*
 sudo sh -c "echo '%firesim ALL=(ALL) NOPASSWD: /usr/local/bin/firesim-*' > /etc/sudoers.d/firesim"
 sudo chmod 400 /etc/sudoers.d/firesim
 
-echo "2. Add your user to the firesim group"
-sudo usermod -a -G firesim $USER
-sudo -l
-
-sudo reboot -h now
