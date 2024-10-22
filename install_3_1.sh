a66cca338d2aa8fe59454147dd035aeac3b0b4ce
Dongho Park <parkdongho28@naver.com>
2024-10-23 00:47:29 +0900

    Update and rename install_3.sh to install_3_1.sh

--- install_3_1.sh
+++ install_3_1.sh
@@ -5,6 +5,3 @@ sudo ./xsetup --batch Install --agree XilinxEULA,3rdPartyEULA --edition "Vivado 
 
 echo "source /tools/Xilinx/Vivado_Lab/2023.1/settings64.sh" >> ~/.bashrc
 
-source ~/.bashrc
-cd /tools/Xilinx/Vivado_Lab/2023.1/data/xicom/cable_drivers/lin64/install_script/install_drivers/
-sudo ./install_drivers
